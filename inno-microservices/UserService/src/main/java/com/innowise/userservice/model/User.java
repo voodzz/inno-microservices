a1043d9cd659249e3c2b02549aac472eb1c93e2c
Ruslan Vaitukevich <rusikruslanchik@mail.ru>
2025-09-18 13:10:09 +0300

    User entity added

---
+++ inno-microservices/UserService/src/main/java/com/innowise/userservice/model/User.java
@@ -0,0 +1,40 @@
+package com.innowise.userservice.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @OneToMany
+    List<Card> cards = new ArrayList<>();
+}
