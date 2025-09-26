5b63e89caa7501ce2759d75de70f36d9c92cca2b
Ruslan Vaitukevich <rusikruslanchik@mail.ru>
2025-09-26 19:23:14 +0300

    USR-5: add serialization IDs

--- inno-microservices/UserService/src/main/java/com/innowise/userservice/exception/NotFoundException.java
+++ inno-microservices/UserService/src/main/java/com/innowise/userservice/exception/NotFoundException.java
@@ -3,8 +3,13 @@ package com.innowise.userservice.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.io.Serial;
+
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Entity not found")
 public class NotFoundException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 2283290493219293067L;
     private static final String MESSAGE_TEMP = "Entity with id '%d' not found";
 
     public NotFoundException() {
--- inno-microservices/UserService/src/main/java/com/innowise/userservice/exception/UpdateException.java
+++ inno-microservices/UserService/src/main/java/com/innowise/userservice/exception/UpdateException.java
@@ -1,6 +1,11 @@
 package com.innowise.userservice.exception;
 
+import java.io.Serial;
+
 public class UpdateException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1193729871561656374L;
     private static final String MESSAGE_TEMP = "Update failed for entity with id '%d'";
 
     public UpdateException() {
