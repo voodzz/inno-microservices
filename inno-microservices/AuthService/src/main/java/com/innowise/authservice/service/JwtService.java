f12ff20567c94d88b2fbafd6595e0a503cfc01b1
Ruslan Vaitukevich <rusikruslanchik@mail.ru>
2025-10-07 22:26:08 +0300

    add securityConfig

--- inno-microservices/AuthService/src/main/java/com/innowise/authservice/service/JwtService.java
+++ inno-microservices/AuthService/src/main/java/com/innowise/authservice/service/JwtService.java
@@ -4,7 +4,6 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,7 +15,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
-@Slf4j
 @Service
 public class JwtService {
   @Value("${security.jwt.secret-key}")
