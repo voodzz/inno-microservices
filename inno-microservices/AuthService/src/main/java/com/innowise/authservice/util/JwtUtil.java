4922637242dbf8e2d0b2485faff00a27d4cf3ed7
Ruslan Vaitukevich <rusikruslanchik@mail.ru>
2025-10-07 13:30:32 +0300

    add JwtUtil

---
+++ inno-microservices/AuthService/src/main/java/com/innowise/authservice/util/JwtUtil.java
@@ -0,0 +1,82 @@
+package com.innowise.authservice.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Slf4j
+@Component
+public class JwtUtil {
+  @Value("${security.jwt.secret-key}")
+  private String secretKey;
+
+  @Value("${security.jwt.access-expiration-time}")
+  private Long accessTokenExpiration;
+
+  @Value("${security.jwt.refresh-expiration-time}")
+  private Long refreshTokenExpiration;
+
+  public String generateAccessToken(UserDetails userDetails) {
+    Map<String, Object> claims = new HashMap<>();
+    return createToken(claims, userDetails.getUsername(), accessTokenExpiration);
+  }
+
+  public String generateRefreshToken(UserDetails userDetails) {
+    Map<String, Object> claims = new HashMap<>();
+    return createToken(claims, userDetails.getUsername(), refreshTokenExpiration);
+  }
+
+  private String createToken(Map<String, Object> claims, String subject, Long expiration) {
+    return Jwts.builder()
+        .claims(claims)
+        .subject(subject)
+        .issuedAt(new Date(System.currentTimeMillis()))
+        .expiration(new Date(System.currentTimeMillis() + expiration))
+        .signWith(getSigningKey())
+        .compact();
+  }
+
+  public Boolean validateToken(String token) {
+    try {
+      Jwts.parser().verifyWith(getSigningKey()).build().parseSignedClaims(token);
+      return true;
+    } catch (JwtException | IllegalArgumentException e) {
+      log.error("JWT token validation failed: {}", e.getMessage());
+      return false;
+    }
+  }
+
+  public String extractUsername(String token) {
+    return extractClaim(token, Claims::getSubject);
+  }
+
+  public Date extractExpiration(String token) {
+    return extractClaim(token, Claims::getExpiration);
+  }
+
+  public <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+    Claims claims = extractAllClaims(token);
+    return claimResolver.apply(claims);
+  }
+
+  private Claims extractAllClaims(String token) {
+    return Jwts.parser().verifyWith(getSigningKey()).build().parseSignedClaims(token).getPayload();
+  }
+
+  private SecretKey getSigningKey() {
+    byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+    return Keys.hmacShaKeyFor(keyBytes);
+  }
+}
