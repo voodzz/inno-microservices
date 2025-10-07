48f48b727a61446d5ba87701364da9a6005bedd5
Ruslan Vaitukevich <rusikruslanchik@mail.ru>
2025-10-07 16:52:39 +0300

    add jwtFilter and userService

--- inno-microservices/AuthService/src/main/java/com/innowise/authservice/service/JwtService.java
+++ inno-microservices/AuthService/src/main/java/com/innowise/authservice/service/JwtService.java
@@ -1,14 +1,14 @@
-package com.innowise.authservice.util;
+package com.innowise.authservice.service;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
@@ -17,8 +17,8 @@ import java.util.Map;
 import java.util.function.Function;
 
 @Slf4j
-@Component
-public class JwtUtil {
+@Service
+public class JwtService {
   @Value("${security.jwt.secret-key}")
   private String secretKey;
 
@@ -30,6 +30,9 @@ public class JwtUtil {
 
   public String generateAccessToken(UserDetails userDetails) {
     Map<String, Object> claims = new HashMap<>();
+    claims.put(
+        "roles",
+        userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList());
     return createToken(claims, userDetails.getUsername(), accessTokenExpiration);
   }
 
@@ -48,14 +51,13 @@ public class JwtUtil {
         .compact();
   }
 
-  public Boolean validateToken(String token) {
-    try {
-      Jwts.parser().verifyWith(getSigningKey()).build().parseSignedClaims(token);
-      return true;
-    } catch (JwtException | IllegalArgumentException e) {
-      log.error("JWT token validation failed: {}", e.getMessage());
-      return false;
-    }
+  public boolean isTokenValid(String token, UserDetails userDetails) {
+    final String username = extractUsername(token);
+    return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+  }
+
+  private boolean isTokenExpired(String token) {
+    return extractExpiration(token).before(new Date());
   }
 
   public String extractUsername(String token) {
@@ -72,7 +74,11 @@ public class JwtUtil {
   }
 
   private Claims extractAllClaims(String token) {
-    return Jwts.parser().verifyWith(getSigningKey()).build().parseSignedClaims(token).getPayload();
+    return Jwts.parser()
+            .verifyWith(getSigningKey())
+            .build()
+            .parseSignedClaims(token)
+            .getPayload();
   }
 
   private SecretKey getSigningKey() {
