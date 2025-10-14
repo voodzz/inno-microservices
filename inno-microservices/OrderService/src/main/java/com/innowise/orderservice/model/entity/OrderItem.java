f69b39cf8b7d9f1331ae974176408a04ca570cec
Ruslan Vaitukevich <rusikruslanchik@mail.ru>
2025-10-14 13:02:19 +0300

    ORD-3: add entities

---
+++ inno-microservices/OrderService/src/main/java/com/innowise/orderservice/model/entity/OrderItem.java
@@ -0,0 +1,56 @@
+package com.innowise.orderservice.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Represents a junction entity between orders and items.
+ *
+ * <p>An order item connects an {@link Order} with an {@link Item} and records the specific quantity
+ * of the item included in that order. This entity serves as the link in the many-to-many
+ * relationship between orders and items.
+ *
+ * @see Order
+ * @see Item
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "order_id")
+  private Order order;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "item_id")
+  private Item item;
+
+  @Column(nullable = false)
+  private Integer quantity;
+
+  public void setOrder(Order order) {
+    this.order = order;
+    order.getOrderItems().add(this);
+  }
+
+  public void setItem(Item item) {
+    this.item = item;
+    item.getOrderItems().add(this);
+  }
+}
