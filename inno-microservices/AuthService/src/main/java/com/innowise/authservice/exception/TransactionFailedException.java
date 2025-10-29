c46a712110044f60658c058a60889dcf58dcef24
Ruslan Vaitukevich <rusikruslanchik@mail.ru>
2025-10-29 20:07:44 +0300

    API: move CardDto, RegistrationRequest, UserServiceDto and TransactionFailedException to AuthService

---
+++ inno-microservices/AuthService/src/main/java/com/innowise/authservice/exception/TransactionFailedException.java
@@ -0,0 +1,25 @@
+package com.innowise.authservice.exception;
+
+import java.io.Serial;
+
+/**
+ * Custom runtime exception thrown when a multiservice transaction fails and requires a rollback.
+ * This is used to signal a composite failure during operations like user registration.
+ */
+public class TransactionFailedException extends RuntimeException {
+  @Serial private static final long serialVersionUID = 1032498112934032841L;
+
+  public TransactionFailedException() {}
+
+  public TransactionFailedException(String message) {
+    super(message);
+  }
+
+  public TransactionFailedException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public TransactionFailedException(Throwable cause) {
+    super(cause);
+  }
+}
