eb54ca1617ec7d5067e643ca86b77dce0fa9af89
Ruslan Vaitukevich <rusikruslanchik@mail.ru>
2025-11-05 17:29:54 +0300

    PAY-3: add DTO

--- inno-microservices/PaymentService/src/main/java/com/innowise/paymentservice/model/StatusEnum.java
+++ inno-microservices/PaymentService/src/main/java/com/innowise/paymentservice/model/StatusEnum.java
@@ -4,9 +4,6 @@ package com.innowise.paymentservice.model;
  * Defines the possible states of a payment transaction.
  */
 public enum StatusEnum {
-    PENDING,
     SUCCESS,
     FAILED,
-    CANCELED,
-    REFUNDED
 }
---
+++ inno-microservices/PaymentService/src/main/java/com/innowise/paymentservice/model/dto/PaymentDto.java
@@ -0,0 +1,28 @@
+package com.innowise.paymentservice.model.dto;
+
+import com.innowise.paymentservice.model.StatusEnum;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+/**
+ * A Data Transfer Object (DTO) representing the immutable data structure for a payment transaction.
+ * Used primarily for transferring payment data between service layers and over API boundaries.
+ *
+ * @param id The unique identifier of the payment. Optional on creation.
+ * @param orderId The ID of the associated order. Must not be null.
+ * @param userId The ID of the user initiating the payment. Must not be null.
+ * @param status The current status of the payment. Must not be null.
+ * @param timestamp The creation or update timestamp. Must be a time in the past or the present.
+ * @param paymentAmount The monetary value of the payment. Must not be null.
+ */
+public record PaymentDto(
+        String id,
+        @NotNull Long orderId,
+        @NotNull Long userId,
+        @NotNull StatusEnum status,
+        @PastOrPresent Instant timestamp,
+        @NotNull BigDecimal paymentAmount
+) {}
